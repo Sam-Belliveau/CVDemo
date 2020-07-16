1b8ef61ac5fa02be17d9bfe62398f972557dc25b
SamBelliveau <sam.belliveau@gmail.com>
2020-07-16 16:04:54 -0400

    fixed target

--- src/main/java/CVDemo/commands/AlignmentCommand.java
+++ src/main/java/CVDemo/commands/AlignmentCommand.java
@@ -1,35 +1,45 @@
 package CVDemo.commands;
 
-import com.stuypulse.stuylib.math.Angle;
-
 import CVDemo.Limelight;
 import CVDemo.Robot;
 
-/**
- * This is the part we are going to edit
- */
+import com.stuypulse.stuylib.control.PIDController;
+
 public class AlignmentCommand {
+
+    public static final double TARGET_DISTANCE = 5;
+    public static final double TARGET_ANGLE = 0;
     
+    private PIDController angleController;
+    private PIDController distanceController;
+
     public Robot robot;
     public Limelight limelight;
 
     public AlignmentCommand(Robot r, Limelight l) {
         robot = r;
         limelight = l;
+        
+        angleController = new PIDController(0.02, 0, 0.002);
+        distanceController = new PIDController(0.2, 0 , 0.04);
+     }
+
+
+     public void execute() { 
+        double angle = 0;
+        double speed = 0;
+        
+        if(limelight.isVisible()) {
+            double angleError = limelight.getTargetXAngle() - TARGET_ANGLE;
+    
+            angle = angleController.update(angleError);
+
+            double distanceError = limelight.getTargetDistance() - TARGET_DISTANCE;
+         
+            speed = distanceController.update(distanceError);
+        
+        } 
+
+        robot.arcadeDrive(speed, angle);
     }
-
-    public void execute() {
-
-        double error = limelight.getTargetXAngle();
-
-        System.out.println(error);
-
-        if(error < 0) {
-            robot.arcadeDrive(0, -1);
-        } else {
-            robot.arcadeDrive(0, 1);
-        }
-    }
-
-
 }
